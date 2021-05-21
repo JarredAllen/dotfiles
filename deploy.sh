6fd8218bd43bd21f1f4547bfdbca4df0f1952883
JarredAllen <jarredallen73@gmail.com>
2021-05-21 15:36:11 -0700

    Set up vim package managers

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ HOME_DOTFILES="bash_aliases bashrc tmux.conf vimrc"
 
 HERE=$(dirname "$0")
 
+# Copy dotfiles from here to the home directory
 if [ -n "$1" ]; then
     HOME="$1"
 fi
@@ -12,3 +13,11 @@ echo "Deploying to $HOME..."
 for file in $HOME_DOTFILES; do
     cp -uv "$HERE/$file" "$HOME/.$file"
 done
+
+# Set up vim to work as desired
+# Set up vundle
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+# Set up plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
