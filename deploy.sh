3013da45c9113d3b69e2978e171a252cdcda0a01
Jarred Allen <jarred@moveparallel.com>
2021-06-09 08:42:18 -0700

    Automatically install vim plugins

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,19 @@ HOME_DOTFILES="bash_aliases bashrc tmux.conf vimrc"
 
 HERE=$(dirname "$0")
 
+# cp doesn't work with the -u flag on mac
 CP_ARGS="-vu"
 if [ "Darwin" = "$(uname -s)" ]; then
     CP_ARGS="-v"
 fi
 
-# Copy dotfiles from here to the home directory
+# Allow the target directory to be changed from default if desired
+# (defaults to user's home directory)
 if [ -n "$1" ]; then
     HOME="$1"
 fi
 echo "Deploying to $HOME..."
-
+# Copy dotfiles from here to the home directory
 for file in $HOME_DOTFILES; do
     cp $CP_ARGS "$HERE/$file" "$HOME/.$file"
 done
@@ -26,3 +28,8 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 fi
 # Set up plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Use Vundle and Plug to install plugins
+vim -S <(cat <<EOF
+PlugInstall
+VundleInstall
+EOF)
